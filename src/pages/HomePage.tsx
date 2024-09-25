3b8ffe77e596109e23e8ad50bfdd542dd2af3c03
Nuelo <akalo.emmanuel18@gmail.com>
2024-09-25 08:19:51 +0100

    update homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
-import egusi from '../assets/soupbowl.com.png';
-import fried from '../assets/fried-rice.png';
-import chicken from '../assets/chicken.png';
-import pie from '../assets/pie.png';
-import drinks from '../assets/assorted-drinks.png';
+import egusi from '../assets/fast-delivery.png';
+import fried from '../assets/hero-img.png';
+import chicken from '../assets/payment.png';
+import pie from '../assets/food.png';
+import drinks from '../assets/few-click.png';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
@@ -10,44 +10,44 @@ const HomePage = () => {
     <div className="flex flex-col gap-12">
       {/* Header section with title and subtitle */}
       <div className="w-[80%] self-center bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16 z-10">
-        <h1 className="text-4xl font-bold tracking-tight text-yellow-800">Tuck into a takeaway</h1>
-        <span className="text-xl font-bold text-yellow-950">Bring Varieties with Clicks</span>
+        <h1 className="text-4xl font-bold tracking-tight text-yellow-950">WHY J40-TRAY</h1>
+        <span className="text-xl font-bold text-yellow-950">"J40-Tray brings a variety of cuisines and restaurants right to you on a platter. Experience the best flavors around—try J40-Tray today!"</span>
       </div>
 
       {/* Grid section displaying different categories */}
       <div className="grid md:grid-cols-5 gap-10">
         <div className="text-center">
           <img src={egusi} alt="" />
-          <Link to="/AfricaDishesPage" className="font-bold text-yellow-800 text-2xl">
-            African dishes
+          <Link to="/AfricaDishesPage" className="font-bold text-yellow-900 text-1.5xl">
+            Fast Delivery
           </Link>
         </div>
 
         <div className="text-center">
           <img src={fried} />
-          <Link to="/ForeignDishesPage" className="font-bold text-yellow-800 text-2xl ">
-            Other dishes
+          <Link to="/ForeignDishesPage" className="font-bold text-yellow-900 text-1.5xl ">
+            Unlimited Options
           </Link>
         </div>
 
         <div className="text-center">
           <img src={chicken} alt="" />
-          <Link to="/AddOnPage" className="font-bold text-yellow-800 text-2xl">
-            Add On
+          <Link to="/AddOnPage" className="font-bold text-yellow-900 text-1.5xl">
+            Easy Payment
           </Link>
         </div>
 
         <div className="text-center ">
           <img src={pie} alt="" />
-          <Link to="/SnackPage" className="font-bold text-yellow-800 text-2xl">
-            Snacks
+          <Link to="/SnackPage" className="font-bold text-yellow-900 text-1.5xl">
+            Only the Best
           </Link>
         </div>
 
         <div className="text-center">
           <img src={drinks} alt="" />
-          <Link to="/DrinksPage" className="font-bold text-yellow-800 text-2xl">
-            Drinks
+          <Link to="/DrinksPage" className="font-bold text-yellow-900 text-1.5xl">
+            Few Clicks Away
           </Link>
         </div>
       </div>
